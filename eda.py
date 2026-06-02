c92ffc9450fa2f5d4b5693aa82cac9e8b60d9fd8
SDineshkumar-developer <sdineshkumar0715@gmail.com>
2026-06-02 18:58:38 +0530

    Add files via upload

---
+++ eda.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.read_csv("students.csv")
+
+print("Dataset:")
+print(df)
+
+print("\nAverage Marks:")
+print(df["Marks"].mean())
+
+print("\nHighest Marks:")
+print(df["Marks"].max())
+
+print("\nLowest Marks:")
+print(df["Marks"].min())
+import matplotlib.pyplot as plt
+
+df.plot(x="Name", y="Marks", kind="bar")
+
+plt.savefig("marks_chart.png")
+
+print("Chart saved successfully!")
